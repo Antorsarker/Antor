2a6b32ce705c6a90ba463d43d653616dbf0caf0e
Antor Kumar Sarker <45475803+Antorsarker@users.noreply.github.com>
2019-02-12 22:58:01 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/user/mylife/MainActivity.java
+++ app/src/main/java/com/example/user/mylife/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String value=countrynames[position];
                 Toast.makeText(MainActivity.this,value,Toast.LENGTH_SHORT).show();
-                //Edit hoica
+                
             }
         });
 
